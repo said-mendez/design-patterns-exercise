5d9451275109d44623b95f5e3f544384aac5f132
Said Mendez <eduardo.said.mndz@gmail.com>
2023-03-25 20:46:42 -0600

    First commit

--- lib/5-install-libraries.bat
+++ lib/5-install-libraries.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=verifone-data-1.0-SNAPSHOT.jar -DgroupId=training.ood -DartifactId=verifone-data -Dversion=1.0-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=./lib/verifone-data-1.0-SNAPSHOT.jar -DgroupId=training.ood -DartifactId=verifone-data -Dversion=1.0-SNAPSHOT -Dpackaging=jar
---
+++ src/main/java/oop/inheritance/Terminal.java
@@ -0,0 +1,4 @@
+package oop.inheritance;
+
+public interface Terminal {
+}
